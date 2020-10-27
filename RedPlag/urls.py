5ffd2cd9170fd9be1fb5730ef94c68f310969821
Manoj137 <190050016@iitb.ac.in>
2020-10-27 23:15:13 +0530

    last but one commit

--- RedPlag/urls.py
+++ RedPlag/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     #path('api/', include('api.urls')),
     path('file/', include('fileupload.urls')),
    # path('api-auth/', include('rest_framework.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name ='login'), 
+    path('login/', views.login_view, name ='login'), 
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),       
     path('home/', views.home, name='home'),
     path('', auth_views.LoginView.as_view(template_name='login.html'), name ='login'),
