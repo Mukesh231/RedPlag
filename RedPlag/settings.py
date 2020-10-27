228803ab1659307430c2701f46e2459c13870ee3
Manoj137 <190050016@iitb.ac.in>
2020-10-27 19:22:14 +0530

    i keep pushing

--- RedPlag/settings.py
+++ RedPlag/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['localhost',
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,13 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'corsheaders',
     'fileupload',
     'accounts',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- RedPlag/urls.py
+++ RedPlag/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name ='login'), 
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),       
     path('home/', views.home, name='home'),
-    path('', views.start, name='start'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name ='login'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
 
