69fdde655e828b5a8463a5554c1307fd8f1d3993
Manoj137 <190050016@iitb.ac.in>
2020-10-28 15:59:04 +0530

    yes

--- accounts/models.py
+++ accounts/models.py
@@ -11,5 +11,5 @@ class CustomUser(AbstractUser):
     org_code = models.CharField(max_length=200, verbose_name=("Organisation password"))
 
 class Organisation(models.Model):
-    organame = models.CharField(max_length=200)
-    orgacode = models.CharField(max_length=200)
+    organame = models.CharField(max_length=200, verbose_name=("Organisation name"))
+    orgacode = models.CharField(max_length=200, verbose_name=("Organisation password"))
