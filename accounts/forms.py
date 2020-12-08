972ebf2a6c60a2362fadb10828273cfb85ae55f1
Savyasach Reddy <savyasach@cse.iitb.ac.in>
2020-12-08 21:00:15 +0530

    accounts documentation

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,23 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    """!
+    
+    Inherits custom UserCreationForm from django.contrib.auth.forms
+    
+    Adds extra fields to facilitate user registration using organisation name and passcode
+   
+    """
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ("username", "password1", "password2", "org_name", "org_code")
 
 class CustomUserChangeForm(UserChangeForm):
+    """!
+
+    Inherits custom UserChangeForm from django.contrib.auth.forms
+
+    """
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
 class CustomUserManager(UserManager):
+    """!
+    Inherits the default UserManager class from django.contrib.auth.models
+    """
     pass
 
 class CustomUser(AbstractUser):
+    """!
+    Inherits the default AbstractUser class from django.contrib.auth.models
+
+    In addition, two more fields are added to the existing user model to facilitate authentication using organisation name and passcode during registration
+    """
     org_name = models.CharField(max_length=200, verbose_name=("Organisation name"))
     org_code = models.CharField(max_length=200, verbose_name=("Organisation password"))
 
 class Organisation(models.Model):
+    """!
+    Custom model to store registered organisation details.   
+    
+    """
     organame = models.CharField(max_length=200, verbose_name=("Organisation name"))
     orgacode = models.CharField(max_length=200, verbose_name=("Organisation password"))
